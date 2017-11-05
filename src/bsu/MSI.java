2eb5efa9bee65f3d901d05e1b946f665f10e8bb2
Dmitry Kobzev <dmitriykobzev99@gmail.com>
2017-11-05 22:51:43 +0300

    В доработке

--- src/bsu/MSI.java
+++ src/bsu/MSI.java
@@ -23,4 +23,142 @@ public class MSI {
         nevyazki=new double [N];
         norm_nevyazki=0;
     }
+
+    ////////////////////Вычисление матрицы B для метода ПИ
+    public void findMatrixBforSimpleIterations(){
+      //Транспонированная матрица
+      double [][] aT=new double[N][N];
+      for(int i=0;i<N;i++){
+          for(int j=0;j<N;j++){
+              aT[i][j]=a[j][i];
+          }
+      }
+
+        System.out.println("Транспонированная матрица aT: ");
+       printMatrix(aT,N);
+
+        //умножаем транспонированную матрицу на исходную
+        System.out.println("Симметричная матрица aT*a:");
+        double [][] symmetricMatrix=new double[N][N];
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                for(int k=0;k<N;k++){
+                    symmetricMatrix[i][j] += aT[i][k]*a[k][j];
+                }
+            }
+        }
+       printMatrix(symmetricMatrix,N);
+
+        double normSymmetricMatrix=calcNormuMatrix(symmetricMatrix,N);
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                if(i != j){
+                    b[i][j] = (-1) * a[i][j] * (1/normSymmetricMatrix);
+                }
+                if(i == j){
+                    b[i][j] = 1 - a[i][j] * (1/normSymmetricMatrix);
+                }
+            }
+        }
+        System.out.println("Матрица b:");
+        printMatrix(b,N);
+    }
+
+    public void printMatrix(double [][]m, int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.println(m[i][j]+ " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void printVector(double [] v , int n){
+        for(int i=0;i<n;i++){
+            System.out.println(v[i]+ " ");
+        }
+    }
+
+    ////////////////////Вычисление вектора g для метода ПИ
+    public void findVectorGforSimpleIterations(){
+        //Транспонированная матрица
+        double [][] aT=new double[N][N];
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                aT[i][j]=a[j][i];
+            }
+        }
+
+        //умножаем транспонированную матрицу на исходную
+        double [][] symmetricMatrix=new double[N][N];
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                for(int k=0;k<N;k++){
+                    symmetricMatrix[i][j] += aT[i][k]*a[k][j];
+                }
+            }
+        }
+
+        double  normSymmetricMatrix =calcNormuMatrix(symmetricMatrix,N);
+        for(int i=0; i < N;i++){
+            g[i]=f[i] * (1/normSymmetricMatrix);
+        }
+        System.out.println("Вектор g: ");
+        printVector(g,N);
+    }
+
+    ////////////////////Вычисление матрицы B для метода Г-З
+    public void findMatrixBforG_Z(){
+
+        for(int i=0; i < N; i++){
+            for(int j=0;j<N;j++){
+                if(i == j){
+                    b[i][i]= 0;
+                }
+                if(i != j){
+                    b[i][j] = -a[i][j] / a[i][i];
+                }
+            }
+        }
+
+        System.out.println("Матрица b: ");
+        printMatrix(b,N);
+    }
+
+    ////////////////////Вычисление вектора g для метода Г-З
+    public void findVectorGforG_Z(){
+        for(int i=0;i<N;i++){
+            g[i] = f[i] / a[i][i];
+        }
+
+        System.out.println("Вектор g: ");
+        printVector(g,N);
+    }
+
+    ////////////////////Вычисление нормы вектора
+    public double calcNormuVectora(double []v ,int n ){
+        double norma = 0;
+        for(int i=0;i<n;i++){
+            if(Math.abs(v[i]) >= norma){
+                norma=v[i];
+            }
+        }
+        return norma;
+    }
+
+    ////////////////////Вычисление нормы матрицы
+    public double calcNormuMatrix(double [][] m,int n){
+        double norma=0;
+        double tmp;
+        for(int i=0;i<n;i++){
+            tmp=0;
+            for(int j=0;j<n;j++){
+                tmp += Math.abs(m[i][j]);
+            }
+            if(tmp>norma){
+                norma=tmp;
+            }
+        }
+        return norma;
+    }
 }
---
+++ src/bsu/Main.java
@@ -0,0 +1,8 @@
+package bsu;
+
+public class Main {
+    public static void main(String[] args) {
+        MSI call=new MSI();
+        call.findMatrixBforSimpleIterations();
+    }
+}
